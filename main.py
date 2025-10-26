0dcea80f07471ace26112050d6d95d88efa9812d
jacob-rushing <jacobrushing12@gmail.com>
2025-10-25 23:56:01 -0500

    no more hard linked file paths

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import sys
 from stats import *
-def main():
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print (f"Found {word_amount} total words")
-    print("--------- Character Count -------")
-    for i in sorted_list:
-        print(f"{i['char']}: {i['num']}")
-        
-main()  
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    def main():
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {file_path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {word_amount} total words")
+        print("--------- Character Count -------")
+        for i in sorted_list:
+            print(f"{i['char']}: {i['num']}")
+                
+    main() 
--- stats.py
+++ stats.py
@@ -1,13 +1,22 @@
+import sys
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    file_path = sys.argv[1]
+
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
         return (file_contents)
     
-text = get_book_text("books/frankenstein.txt")
+text = get_book_text(file_path)   
 words = text.split()
 character_count = {}
 sorted_list = []
 word_amount = 0
+
+
 
 def get_num_words():
    global word_amount
